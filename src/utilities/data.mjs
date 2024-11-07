81c1545eb336fb647d8a92b5a6e32b4f38541357
SWelbeck <swelbeck12@gmail.com>
2024-11-07 15:49:05 -0500

    added initial data

---
+++ src/utilities/data.mjs
@@ -0,0 +1,124 @@
+const initialState = [
+  {
+    userId: 1,
+    id: 1,
+    title: "Go grocery shopping",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "Do laundry",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "Return package",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: "Study for exam",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 5,
+    title: "Read that book",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 6,
+    title: "Go to the gym",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 7,
+    title: "Make eye doctor appointment",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 8,
+    title: "Make Dentist appointment",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 9,
+    title: "Reschedule therapy",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 10,
+    title: "Find a new therapist",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 11,
+    title: "Apply for 5 jobs",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 12,
+    title: "Re-organize shelf",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 13,
+    title: "Cook dinner",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 14,
+    title: "Meal prep for upcoming week",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 15,
+    title: "Update portfolio",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 16,
+    title: "Organize email inbox",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 17,
+    title: "Go for a walk",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 18,
+    title: "Learn guitar",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 19,
+    title: "Practice keyboard",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 20,
+    title: "JS algorithm practice",
+    completed: true,
+  },
+];
+
+export default initialState;
